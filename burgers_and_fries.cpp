19c08e09756b6fab570cc32bb77f57827ceb7e1e
Mackay <mackeyfisher@gmail.com>
2023-11-09 16:01:34 +0000

    started with the sem class however it print's out of order so will add queue logic after work

--- burgers_and_fries.cpp
+++ burgers_and_fries.cpp
@@ -17,8 +17,7 @@ int k;
 void process_order() {
     sleep(2);
 }
-
-void place_order(int type) {
+void place_order(int type,sem_t& ordersync) {
     /**
      *  Add logic for synchronization before order processing
      *  Check if already k orders in process; 
@@ -26,23 +25,37 @@ void place_order(int type) {
      *     otherwise place this order (print order)
      *  Use type_names[type] to print the order type
      */
+    int sem_value;
+    sem_getvalue(&ordersync, &sem_value);
+    if(sem_value <= 0){
+        cout << "Waiting: " << type_names[type] << endl;
+    }
+    
+    // Wait for semaphore to allow processing
+    sem_wait(&ordersync);
+    cout << "Order: " << type_names[type] << endl;
 
-    process_order();        // Do not remove, simulates preparation
+    process_order(); // Process the order
+    
+    sem_post(&ordersync); // Signal that the order is processed
+    //process_order();        // Do not remove, simulates preparation
 
     /**
      *  Add logic for synchronization after order processed
      *  Allow next order of the same type to proceed if there is any waiting; if not, allow the other type to proceed.
      */
+    //sem_post(&ordersync);
 }
 
 int main() {
     // Initialize necessary variables, semaphores etc.
-    
+    sem_t ordersync;
     // Read data: done for you, do not change
     pii incoming[MAX_THREADS];
     int _type, _arrival;
     int t;
     cin >> k;
+        sem_init(&ordersync, 0, k);
     cin >> t;
     for (int i = 0; i < t; ++i) {
         cin >> _type >> _arrival;
@@ -54,7 +67,7 @@ int main() {
     thread* threads[MAX_THREADS];
     for (int i = 0; i < t; ++i) {
         _type = incoming[i].first;
-        threads[i] = new thread(place_order, _type);
+        threads[i] = new thread(place_order, _type, std::ref(ordersync));
         if (i < t - 1) {
             int _sleep = incoming[i + 1].second - incoming[i].second;
             sleep(_sleep);
@@ -66,5 +79,6 @@ int main() {
         threads[i]->join();
         delete threads[i];
     }
+    sem_destroy(&ordersync);
     return 0;
 }
